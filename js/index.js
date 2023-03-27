3f5d1782d567301dc12d3e09706f9224d0b1b982
Gautham Venkateshwaran <gauthamv6002@gmail.com>
2023-03-26 23:59:37 -0600

    made typewriter better

--- js/index.js
+++ js/index.js
@@ -4,21 +4,21 @@ const retype1 = new Typed('#retype1', {
     strings: ['hello', 'researchers^2000', 'businesses^2000', 'clients'],
     typeSpeed: 50,
     startDelay: 2000,
-    backSpeed: 50,
+    backSpeed: 0,
 });
 
 const retype2 = new Typed('#retype2', {
     strings: ['students', 'thinkers^2000', 'innovators^2000', 'developers'],
     typeSpeed: 50,
     startDelay: 2000,
-    backSpeed: 50,
+    backSpeed: 0,
 });
 
 const retype3 = new Typed('#retype3', {
     strings: ['tools', 'results^2000', 'solutions^2000', 'the future'],
     typeSpeed: 50,
     startDelay: 2000,
-    backSpeed: 50,
+    backSpeed: 0,
 });
 
 setTimeout(() => {
