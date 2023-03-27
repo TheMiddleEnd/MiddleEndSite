5a4c332c24c07218db4b9b6c30a1deac57513736
Gautham Venkateshwaran <gauthamv6002@gmail.com>
2023-03-26 22:12:37 -0600

    added typewriter effect ⌨️

---
+++ js/index.js
@@ -0,0 +1,26 @@
+//Using typed.js to create typewriter effect
+
+const retype1 = new Typed('#retype1', {
+    strings: ['hello', 'researchers^2000', 'businesses^2000', 'clients'],
+    typeSpeed: 50,
+    startDelay: 2000,
+    backSpeed: 50,
+});
+
+const retype2 = new Typed('#retype2', {
+    strings: ['students', 'thinkers^2000', 'innovators^2000', 'developers'],
+    typeSpeed: 50,
+    startDelay: 2000,
+    backSpeed: 50,
+});
+
+const retype3 = new Typed('#retype3', {
+    strings: ['tools', 'results^2000', 'solutions^2000', 'the future'],
+    typeSpeed: 50,
+    startDelay: 2000,
+    backSpeed: 50,
+});
+
+setTimeout(() => {
+    $("#retype3").addClass("underlineThickR");
+}, 10600);
