4af2261c239f07898372778b3ad8a5a6a2e4e8a0
Gautham Venkateshwaran <gauthamv6002@gmail.com>
2023-03-27 00:09:16 -0600

    fixed typewriter - now in sequence

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
 //Using typed.js to create typewriter effect
 
 const retype1 = new Typed('#retype1', {
-    strings: ['hello', 'researchers^2000', 'businesses^2000', 'clients'],
+    strings: ['teachers', 'researchers^4000', 'businesses^4000', 'clients'],
     typeSpeed: 50,
     startDelay: 2000,
-    backSpeed: 0,
+    backSpeed: 35,
 });
 
 const retype2 = new Typed('#retype2', {
-    strings: ['students', 'thinkers^2000', 'innovators^2000', 'developers'],
+    strings: ['students', 'thinkers^4000', 'innovators^4000', 'developers'],
     typeSpeed: 50,
-    startDelay: 2000,
-    backSpeed: 0,
+    startDelay: 3400,
+    backSpeed: 50,
 });
 
 const retype3 = new Typed('#retype3', {
-    strings: ['tools', 'results^2000', 'solutions^2000', 'the future'],
+    strings: ['tools', 'results^4000', 'solutions^4000', 'the future'],
     typeSpeed: 50,
-    startDelay: 2000,
-    backSpeed: 0,
+    startDelay: 4800,
+    backSpeed: 50,
 });
 
 setTimeout(() => {
     $("#retype3").addClass("underlineThickR");
-}, 10600);
+}, 17350);
