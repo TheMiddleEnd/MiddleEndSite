39278da58deb6984bbfa97e9971a43237fbaf0b6
ericli3690 <ericli3690@gmail.com>
2023-03-28 01:04:20 -0600

    signup system version 1 done problems: does not show success or failure messages has not been bugtested rigorously

--- js/firebase.js
+++ js/firebase.js
@@ -20,8 +20,26 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-set(ref(db, '000000000'), {
-  a: "oneone",
-  b: "two",
-  c: "three"
+$("#signup-button").click(() => {
+
+  const name = $("#name-input").val();
+  const phone = $("#phone-input").val();
+  const email = $("#email-input").val();
+
+  if ([name,phone,email].some((e) => !e)) { // at least one is empty
+    alert("YOU'RE BAD"); // TODO: HANDLE ON ERROR, SHOW ERROR MESSAGE
+    return;
+  }
+
+  // console.log(name, phone, email);
+
+  set(ref(db, email), {
+    name: name,
+    phone: phone
+  });
+
+  alert("YOU'RE GOOD");
+
+  // TODO: HANDLE ON SUCCESS, SHOW SUCCESS MESSAGE
+
 });
